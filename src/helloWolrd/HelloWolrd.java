7c49be5926b08c4f524a712b5d5cec16ac38dbad
qnfgh <qnfgh@DESKTOP-L4HK6TQ>
2021-05-12 10:15:04 +0900

    Hello 메세지 추가

--- src/helloWolrd/HelloWolrd.java
+++ src/helloWolrd/HelloWolrd.java
@@ -5,5 +5,6 @@ public class HelloWolrd {
 	public static void main(String[] args) {
 		System.out.println(" 안녕하세요 Java로 시작합니다.");
 		System.out.println(" 안녕하세요 ");
+		System.out.println(" Hello ");
 	}
 }
